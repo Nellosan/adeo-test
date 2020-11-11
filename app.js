7141f147a1ba05eee3874554f71250e5bbe5795b
Paul Foulont <pafoulont@gmail.com>
2020-11-11 16:26:42 +0100

    testing

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
 const data = require("./data").data;
 const args = process.argv.slice(2);
 
+function filterAnimalsByName(dataFilter, search) {
+  if (dataFilter === undefined || typeof dataFilter !== "object") {
+    throw new Error("You must provide a data object.");
+  }
+
+  if (search === undefined || typeof search !== "string") {
+    throw new Error("You must provide a searching string.");
+  }
+
+  return (
+    dataFilter
+      .map((country) => {
+        return {
+          ...country,
+          people: country.people
+            .map((people) => {
+              return {
+                ...people,
+                animals: people.animals.filter(
+                  // we search for the value in every animal's name
+                  (animal) => animal.name.search(search) > -1
+                ),
+              };
+            })
+            // removing people if empty animals
+            .filter((people) => people.animals.length > 0),
+        };
+      })
+      // removing country if empty people
+      .filter((country) => country.people.length > 0)
+  );
+}
+
+function countAnimalsAndPeople(dataCount) {
+  if (dataCount === undefined || typeof dataCount !== "object") {
+    throw new Error("You must provide a data object.");
+  }
+
+  return dataCount.map((country) => {
+    return {
+      ...country,
+      // changing country's name to add the number of people
+      name: `${country.name} [${country.people.length}]`,
+      people: country.people.map((people) => {
+        return {
+          ...people,
+          // changing people's to add the number of animals
+          name: `${people.name} [${people.animals.length}]`,
+        };
+      }),
+    };
+  });
+}
+
 // Inspect the different args
 args.map(function (a) {
   // Seperate the value from the arg's name
@@ -9,48 +63,21 @@ args.map(function (a) {
   switch (a[0]) {
     // if arg's name is --filter
     case "--filter":
-      const filteredData = data
-        .map((country) => {
-          return {
-            ...country,
-            people: country.people
-              .map((people) => {
-                return {
-                  ...people,
-                  animals: people.animals.filter(
-                    // we search for the value in every animal's name
-                    (animal) => animal.name.search(a[1]) > -1
-                  ),
-                };
-              })
-              // removing people if empty animals
-              .filter((people) => people.animals.length > 0),
-          };
-        })
-        // removing country if empty people
-        .filter((country) => country.people.length > 0);
+      const filteredData = filterAnimalsByName(data, a[1]);
 
       console.log(JSON.stringify(filteredData, null, 2));
       break;
 
     // if arg's name is --count
     case "--count":
-      const countedData = data.map((country) => {
-        return {
-          ...country,
-          // changing country's name to add the number of people
-          name: `${country.name} [${country.people.length}]`,
-          people: country.people.map((people) => {
-            return {
-              ...people,
-              // changing people's to add the number of animals
-              name: `${people.name} [${people.animals.length}]`,
-            };
-          }),
-        };
-      });
+      const countedData = countAnimalsAndPeople(data);
 
       console.log(JSON.stringify(countedData, null, 2));
       break;
   }
 });
+
+module.exports = {
+  filterAnimalsByName,
+  countAnimalsAndPeople,
+};
---
+++ app.spec.js
@@ -0,0 +1,146 @@
+const app = require("./app");
+const data = require("./data");
+const dataTest = require("./data").data;
+
+// -------------------------------------------------------------
+// Testing filter on animals by name
+
+test("Filter animals on 'ry' with undefined data.", () => {
+  expect(() => {
+    app.filterAnimalsByName(undefined, "ry");
+  }).toThrow(Error);
+});
+
+test("Filter animals on 'ry' with empty data.", () => {
+  expect(app.filterAnimalsByName([], "ry")).toStrictEqual([]);
+});
+
+test("Filter animals with undefined searching value.", () => {
+  expect(() => {
+    app.filterAnimalsByName(dataTest);
+  }).toThrow(Error);
+});
+
+test("Filter animals with an array of searching values.", () => {
+  expect(() => {
+    app.filterAnimalsByName(dataTest, ["ry", "og", "iz"]);
+  }).toThrow(Error);
+});
+
+test("Filter animals with empty searching value.", () => {
+  expect(app.filterAnimalsByName(dataTest, "")).toStrictEqual(dataTest);
+});
+
+test("Filter animals with impossible searching value : 'yuzoqksolq'.", () => {
+  expect(app.filterAnimalsByName(dataTest, "yuzoqksolq")).toStrictEqual([]);
+});
+
+test("Filter animals on 'ry'.", () => {
+  expect(app.filterAnimalsByName(dataTest, "ry")).toStrictEqual([
+    {
+      name: "Uzuzozne",
+      people: [
+        {
+          name: "Lillie Abbott",
+          animals: [
+            {
+              name: "John Dory",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Satanwi",
+      people: [
+        {
+          name: "Anthony Bruno",
+          animals: [
+            {
+              name: "Oryx",
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+});
+
+// -------------------------------------------------------------
+// Testing animals and people couting
+
+test("Count animals and people with undefined data.", () => {
+  expect(() => {
+    app.countAnimalsAndPeople();
+  }).toThrow(Error);
+});
+
+test("Count animals and people with empty array of data.", () => {
+  expect(app.countAnimalsAndPeople([])).toStrictEqual([]);
+});
+
+test("Count animals and people with some data.", () => {
+  expect(
+    app.countAnimalsAndPeople([
+      {
+        name: "Dillauti",
+        people: [
+          {
+            name: "Winifred Graham",
+            animals: [
+              { name: "Anoa" },
+              { name: "Duck" },
+              { name: "Narwhal" },
+              { name: "Badger" },
+              { name: "Cobra" },
+              { name: "Crow" },
+            ],
+          },
+          {
+            name: "Blanche Viciani",
+            animals: [
+              { name: "Barbet" },
+              { name: "Rhea" },
+              { name: "Snakes" },
+              { name: "Antelope" },
+              { name: "Echidna" },
+              { name: "Crow" },
+              { name: "Guinea Fowl" },
+              { name: "Deer Mouse" },
+            ],
+          },
+        ],
+      },
+    ])
+  ).toStrictEqual([
+    {
+      name: "Dillauti [2]",
+      people: [
+        {
+          name: "Winifred Graham [6]",
+          animals: [
+            { name: "Anoa" },
+            { name: "Duck" },
+            { name: "Narwhal" },
+            { name: "Badger" },
+            { name: "Cobra" },
+            { name: "Crow" },
+          ],
+        },
+        {
+          name: "Blanche Viciani [8]",
+          animals: [
+            { name: "Barbet" },
+            { name: "Rhea" },
+            { name: "Snakes" },
+            { name: "Antelope" },
+            { name: "Echidna" },
+            { name: "Crow" },
+            { name: "Guinea Fowl" },
+            { name: "Deer Mouse" },
+          ],
+        },
+      ],
+    },
+  ]);
+});
